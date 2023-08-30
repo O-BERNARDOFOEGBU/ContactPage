e180dddb9781b3a37c9fcf329b9ff9c302c4c4c0
O-Bernard Ofoegbu <obinnaofoegbu71@gmail.com>
2023-08-31 00:30:59 +0100

    contact

--- src/Sections/Hero.js
+++ src/Sections/Hero.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import Contact from '../components/Contact'
+import background from "../assets/background.jpg";
+
 const Hero = () => {
   return (
-    <div>
-      <div></div>
-      <Contact/>
-    </div>
+      <div className="background"style={{ backgroundImage: `url(${background})`, backgroundRepeat:"no-repeat", backgroundSize:"contain",  marginTop:"12%", height: "700px" }}>
+        <div className="hero-container" >
+        <div style={{width:"40%"}}>
+          <div>
+            yup
+            uiuui
+          </div>
+          <div>
+            yup
+            uiuui
+          </div>
+          <div>
+            yup
+            uiuui
+          </div>
+        </div>
+        <Contact/>
+        </div>
+      </div>
   )
 }
 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,22 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
-        <div>Contact</div>
-    </div>
+   <div className="contact-container" >
+    <h1>
+      Get in touch
+    </h1>
+    <form>
+      <div>
+      <input type="text" name="firstname" placeholder='First Name'/>
+      <input type="text" name="lastname" placeholder='Last Name'/>
+      </div>
+      <input type="email" name="email" placeholder='Email Address'/>
+      <textarea name="message" placeholder='Describe your issues' rows={10} cols={30}/>
+      <button type="submit">Send</button>
+
+    </form>
+     
+   </div>
    
   )
 }
