7614f72c0256bf05227370afded35515468d3229
O-Bernard Ofoegbu <obinnaofoegbu71@gmail.com>
2023-08-30 13:39:35 +0100

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./Sections/Footer";
+import Hero from "./Sections/Hero";
+import Navigation from "./Sections/Navigation";
+import Newsletter from "./Sections/Newsletter";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Navigation/>
+     <Hero/>
+     <Newsletter/>
+     <Footer/>
     </div>
   );
 }
---
+++ src/Sections/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Contact from '../components/Contact'
+const Hero = () => {
+  return (
+    <div>
+      <div></div>
+      <Contact/>
+    </div>
+  )
+}
+
+export default Hero
