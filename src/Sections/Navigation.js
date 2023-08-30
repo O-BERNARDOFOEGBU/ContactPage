5f6b818ae4d7956484ce3ce6eddbc87bd230f09e
O-Bernard Ofoegbu <obinnaofoegbu71@gmail.com>
2023-08-30 17:30:56 +0100

    navigation process

--- src/Sections/Navigation.js
+++ src/Sections/Navigation.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import {ReactComponent as RareEatLogo} from "../assets/rare-eat-logo.svg"
+import {ReactComponent as SearchLogo} from "../assets/search.svg"
+
 
 const Navigation = () => {
   return (
-    <div>Navigation</div>
+    <div className='nav-header'>
+
+      <div className='logo'>
+       <RareEatLogo/>
+      </div>
+      <div className='nav-links'>
+        <div>store</div>
+        <div>about</div>
+        <div>contact</div>
+        <div>blog</div>
+      </div>
+      <div className="input-field">
+      <span className="search-logo"><SearchLogo/></span>
+        <input className="input" type="text" placeholder="Search" />
+        {/* <img src="/images/search-icon.png" alt="Search Icon" /> */}
+        
+      </div>
+      <div><RareEatLogo/> </div>
+    </div>
   )
 }
 
