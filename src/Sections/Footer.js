291a3d4164bcdf986737595d39f4772099f64c36
O-Bernard Ofoegbu <obinnaofoegbu71@gmail.com>
2023-08-31 05:10:13 +0100

    footer

--- src/Sections/Footer.js
+++ src/Sections/Footer.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import {ReactComponent as RareEatLogo} from "../assets/rare-eat-logo.svg"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <RareEatLogo style={{ width:"110px", height:"59px"}}/>
+      <div className='footer-content'>
+        <ul>
+          <li>Company</li>
+          <li>About Us</li>
+          <li>Store</li>
+          <li>Blog</li>
+        </ul>
+        <ul>
+          <li>Company</li>
+          <li>About Us</li>
+          <li>Store</li>
+          <li>Blog</li>
+        </ul>
+        <ul>
+          <li>Company</li>
+          <li>About Us</li>
+          <li>Store</li>
+          <li>Blog</li>
+        </ul>
+        <ul>
+          <li>Company</li>
+          <li>About Us</li>
+          <li>Store</li>
+          <li>Blog</li>
+        </ul>
+      </div>
+      <hr/>
+      <p style={{color:"#FFFFFF"}}>2023 Rare Eat Fresh. All rights reserved.</p>
+    </div>
   )
 }
 
--- src/Sections/Newsletter.js
+++ src/Sections/Newsletter.js
@@ -8,10 +8,10 @@ const Newsletter = () => {
         Rare Eat Fresh community. Stay connected with the latest 
       </div>
       <div className="newsletter-text">updates, exclusive offers, and exciting promotions on rare West African food ingredients.</div>
-      <div style={{display:"flex", flexDirection:"row", justifyContent:"space-between", gap:"20%"}}>
+      <div style={{display:"flex", flexDirection:"row", justifyContent:"space-between", gap:"2%"}}>
       {/* <div className="news-input-container"> */}
-      <input  type="email" name="email" placeholder='Email address'/>
-      <button  type="submit">Subscribe</button>
+      <input className='news-input' type="email" name="email" placeholder='Email address'/>
+      <button className='news-submit'  type="submit">Subscribe</button>
       {/* </div> */}
       </div>
     </div>
